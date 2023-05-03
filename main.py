94ea562e3dcaf370f35de91b3e947d9c699c3dc7
Kunal Singh <kunals@ip-172-16-21-143.us-west-2.compute.internal>
2023-05-03 13:54:29 -0700

    fix

--- main.py
+++ main.py
@@ -16,12 +16,11 @@ def load_dataset() -> prior.DatasetDict:
     # for split, size in zip(("val", "test"), (100_000, 1_000, 1_000)):
 
     for split in ("val"):
-        if not f"procthor100k_objectnav_{split}.jsonl.gz" in os.listdir("./"):
+        if not f"procthor_100k_objectnav_{split}.jsonl.gz" in os.listdir("./"):
             url = f"https://prior-datasets.s3.us-east-2.amazonaws.com/procthor_100k_eval-kunal/procthor_100k_objectnav_val.jsonl.gz"
             urllib.request.urlretrieve(
                 url, "./procthor_100k_objectnav_{}.jsonl.gz".format(split)
             )
-        print ('gets here?')
         with gzip.open(f"procthor_100k_objectnav_{split}.jsonl.gz", "r") as f:
             tasks = [line for line in tqdm(f, desc=f"Loading {split}")]
         data[split] = LazyJsonDataset(
