652b932bc97fe53228bcc6cc14670841df65ee1d
Kunal Singh <kunals@ip-172-16-21-143.us-west-2.compute.internal>
2023-05-03 13:55:07 -0700

    fix

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def load_dataset() -> prior.DatasetDict:
     data = {}
     # for split, size in zip(("val", "test"), (100_000, 1_000, 1_000)):
 
-    for split in ("val"):
+    for split in ["val"]:
         if not f"procthor_100k_objectnav_{split}.jsonl.gz" in os.listdir("./"):
             url = f"https://prior-datasets.s3.us-east-2.amazonaws.com/procthor_100k_eval-kunal/procthor_100k_objectnav_val.jsonl.gz"
             urllib.request.urlretrieve(
