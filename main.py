01899d4d7db84b5dd37780cd297105d9d160cd61
Kunal Singh <kunals@ip-172-16-21-143.us-west-2.compute.internal>
2023-05-03 13:46:32 -0700

    fix main.py typosa

--- main.py
+++ main.py
@@ -16,13 +16,12 @@ def load_dataset() -> prior.DatasetDict:
     # for split, size in zip(("val", "test"), (100_000, 1_000, 1_000)):
 
     for split in ("val"):
-
         if not f"procthor100k_objectnav_{split}.jsonl.gz" in os.listdir("./"):
             url = f" https://prior-datasets.s3.us-east-2.amazonaws.com/procthor_100k_eval-kunal/procthor_100k_objectnav_{split}.jsonl.gz"
             urllib.request.urlretrieve(
-                url, "./procthor_100k_balanced_{}.jsonl.gz".format(split)
+                url, "./procthor_100k_objectnav_{}.jsonl.gz".format(split)
             )
-        with gzip.open(f"procthor_100k_balanced_{split}.jsonl.gz", "r") as f:
+        with gzip.open(f"procthor_100k_objectnav_{split}.jsonl.gz", "r") as f:
             tasks = [line for line in tqdm(f, desc=f"Loading {split}")]
         data[split] = LazyJsonDataset(
             data=tasks, dataset="procthor-100k-eval", split=split
