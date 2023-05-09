cf79aa3e95293078d5ff77fd5dd57df96b3c747c
roseh-ai2 <roseh@allenai.org>
2023-05-09 14:18:53 -0700

    task registration and templated task language descriptions

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def load_dataset() -> prior.DatasetDict:
 
     for split in ["val", "test"]:
         split_task_list = []
-        for task in ["objectnav", "fetch2room", "objectexplore"]:
+        for task in ["objectnavtype", "fetch2roomtype", "exploreobjects"]:
             filename = f"minival_procthor_100k_{task}_{split}.jsonl.gz"
             print(filename)
             if not filename in os.listdir("./"):
